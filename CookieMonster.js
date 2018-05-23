8e7820a69225002be6cf7fdb4c0890de08398104
Johannes Dachsel <johannes@2fourmedia.de>
2018-05-23 09:58:00 +0200

    extended module configuration

---
+++ CookieMonster.js
@@ -0,0 +1,5 @@
+function setCookieMonster(){
+	document.cookie = "cmnstr=1; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+	var cmnstrBanner = document.querySelector(".cmnstr");
+	cmnstrBanner.remove();
+}
