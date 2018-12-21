76ac866d748c2625877d388727e3d3df30780a81
Johannes Dachsel <johannes@2fourmedia.de>
2018-12-21 08:01:37 +0100

    IE11 fix

--- CookieMonster.js
+++ CookieMonster.js
@@ -1,5 +1,5 @@
 function setCookieMonster(){
 	document.cookie = "cmnstr=1; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 	var cmnstrBanner = document.querySelector(".cmnstr");
-	cmnstrBanner.remove();
+	cmnstrBanner.parentNode.removeChild(cmnstrBanner);
 }
